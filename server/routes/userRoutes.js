2053f241cabf8b9553746436f3f69dc7bada38d8
AhmedZidan399 <ahmed.zidan.new10@gmail.com>
2025-10-23 23:21:34 +0300

    update: user controller to get users with search by name or email

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const {
-  addUser,
-  getUsers,
-  updateUser,
-  deleteUser,
+	addUser,
+	getUsers,
+	updateUser,
+	deleteUser,
+	getUsersWithSearch,
 } = require("../controllers/userController");
-
-// Add new user
-router.post("/add", addUser);
 
 // Get all users
 router.get("/", getUsers);
+
+// Get users with search
+router.get("/search?q=:q", getUsersWithSearch);
+
+// Add new user
+router.post("/add", addUser);
 
 // Update user by ID
 router.put("/:id", updateUser);
